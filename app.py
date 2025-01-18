777d4830e4b9085fb49dc9bdd658401919ae1bdf
Agata Tekien <tekienagata@gmail.com>
2025-01-18 20:44:46 +0100

    adding CRUD for exercises table

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from models import Exercise
+from flask import Flask, jsonify, make_response, request
+from models import db, Exercise
 from os import environ
 
 
 app = Flask(__name__)
-DATABASE_URL = f"postgresql+psycopg2://{environ.get('POSTGRES_USER')}:{environ.get('POSTGRES_PASSWORD')}@{environ.get('POSTGRES_HOST')}:{environ.get('POSTGRES_PORT')}/{environ.get('POSTGRES_DB')}"
+DATABASE_URL = (
+    f"postgresql+psycopg2://{environ.get('POSTGRES_USER')}:"
+    f"{environ.get('POSTGRES_PASSWORD')}@{environ.get('POSTGRES_HOST')}:"
+    f"{environ.get('POSTGRES_PORT')}/{environ.get('POSTGRES_DB')}"
+)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
-dbapp = SQLAlchemy(app)
+db.init_app(app)
 with app.app_context():
-    dbapp.create_all()
+    db.create_all()
 
 
 @app.route("/")
@@ -17,11 +20,61 @@ def hello_world():
     return "Hello World!"
 
 
-"""@app.route("/exercises")
+@app.route("/exercises", methods=["GET"])
 def get_exercises():
-    session.close()
-    exercises = session.query(Exercise).all()
-    return jsonify(exercises)"""
+    try:
+        exercises = Exercise.query.all()
+        return make_response(jsonify([exercise.json() for exercise in exercises]), 200)
+    except Exception as e:
+        return make_response(
+            jsonify({"message": "error getting exercises", "error": str(e)}), 500
+        )
+
+
+@app.route("/exercises", methods=["POST"])
+def create_exercise():
+    try:
+        data = request.get_json()
+        if not all(field in data for field in ["name", "description", "category"]):
+            return make_response(jsonify({"message": "required fields missing"}), 400)
+        exercise = Exercise(
+            name=data["name"], description=data["description"], category=data["category"]
+        )
+        db.session.add(exercise)
+        db.session.commit()
+        return make_response(jsonify({"message": "exercise created successfully"}), 201)
+    except Exception as e:
+        return make_response(
+            jsonify({"message": "error creating exercise", "error": str(e)}), 500
+        )
+
+
+@app.route("/exercises/<int:id>", methods=["PUT", "DELETE"])
+def modify_exercise(id):
+    try:
+        exercise = Exercise.query.get(id)
+        if exercise:
+            if request.method == "DELETE":
+                db.session.delete(exercise)
+                db.session.commit()
+                return make_response(
+                    jsonify({"message": f"exercise {id} deleted"}), 200
+                )
+            if request.method == "PUT":
+                data = request.get_json()
+                if "name" in data:
+                    exercise.name = data["name"]
+                if "description" in data:
+                    exercise.description = data["description"]
+                if "category" in data:
+                    exercise.category = data["category"]
+                db.session.commit()
+                return make_response(
+                    jsonify({"message": f"exercise {id} updated"}), 200
+                )
+        return make_response(jsonify({"message": "exercise not found"}), 404)
+    except Exception as e:
+        make_response(jsonify({"message": f"error modifying exercise {id}","error":str(e)}), 500)
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,12 +1,20 @@
 from sqlalchemy import Column, Integer, String, Text
-from sqlalchemy.ext.declarative import declarative_base
+from flask_sqlalchemy import SQLAlchemy
 
-Base = declarative_base()
+db = SQLAlchemy()
 
 
-class Exercise(Base):
+class Exercise(db.Model):
     __tablename__ = "exercises"
     id = Column(Integer, primary_key=True)
     name = Column(String(255), nullable=False)
     description = Column(Text)
     category = Column(String(100), nullable=False)
+
+    def json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "category": self.category,
+        }
