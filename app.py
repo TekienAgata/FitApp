edc610fcac534f45c846b1e5e7552f061d7e5fea
Agata Tekien <tekienagata@gmail.com>
2025-01-28 17:09:54 +0100

    Adding new models, first steps for expanding db and app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, make_response, request
-from models import db, Exercise
+from models import db, Exercise,User, Workout
 from os import environ
 
 
@@ -20,6 +20,7 @@ def hello_world():
     return "Hello World!"
 
 
+# Exercises' endpoints
 @app.route("/exercises", methods=["GET"])
 def get_exercises():
     try:
@@ -38,7 +39,9 @@ def create_exercise():
         if not all(field in data for field in ["name", "description", "category"]):
             return make_response(jsonify({"message": "required fields missing"}), 400)
         exercise = Exercise(
-            name=data["name"], description=data["description"], category=data["category"]
+            name=data["name"],
+            description=data["description"],
+            category=data["category"],
         )
         db.session.add(exercise)
         db.session.commit()
@@ -74,8 +77,71 @@ def modify_exercise(id):
                 )
         return make_response(jsonify({"message": "exercise not found"}), 404)
     except Exception as e:
-        make_response(jsonify({"message": f"error modifying exercise {id}","error":str(e)}), 500)
+        make_response(
+            jsonify({"message": f"error modifying exercise {id}", "error": str(e)}), 500
+        )
 
+
+# Users' endpoints
+@app.route("/users", methods=["GET"])
+def get_exercises():
+    try:
+        users = User.query.all()
+        return make_response(jsonify([user.json() for user in users]), 200)
+    except Exception as e:
+        return make_response(
+            jsonify({"message": "error getting users", "error": str(e)}), 500
+        )
+
+
+@app.route("/users", methods=["POST"])
+def create_exercise():
+    try:
+        data = request.get_json()
+        if not all(field in data for field in ["name", "description", "category"]):
+            return make_response(jsonify({"message": "required fields missing"}), 400)
+        user = User(
+            name=data["name"],
+            description=data["description"],
+            category=data["category"],
+        )
+        db.session.add(user)
+        db.session.commit()
+        return make_response(jsonify({"message": "user created successfully"}), 201)
+    except Exception as e:
+        return make_response(
+            jsonify({"message": "error creating user", "error": str(e)}), 500
+        )
+
+
+@app.route("/users/<int:id>", methods=["PUT", "DELETE"])
+def modify_exercise(id):
+    try:
+        user = User.query.get(id)
+        if user:
+            if request.method == "DELETE":
+                db.session.delete(user)
+                db.session.commit()
+                return make_response(
+                    jsonify({"message": f"user {id} deleted"}), 200
+                )
+            if request.method == "PUT":
+                data = request.get_json()
+                if "name" in data:
+                    user.name = data["name"]
+                if "description" in data:
+                    user.description = data["description"]
+                if "category" in data:
+                    user.category = data["category"]
+                db.session.commit()
+                return make_response(
+                    jsonify({"message": f"user {id} updated"}), 200
+                )
+        return make_response(jsonify({"message": "user not found"}), 404)
+    except Exception as e:
+        make_response(
+            jsonify({"message": f"error modifying user {id}", "error": str(e)}), 500
+        )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- models.py
+++ models.py
@@ -1,20 +1,66 @@
-from sqlalchemy import Column, Integer, String, Text
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
+
+workout_exercise = db.Table(
+    "workout_exercise",
+    db.Column("workout_id", db.Integer, db.ForeignKey("workouts.id"), primary_key=True),
+    db.Column("exercise.id", db.Integer, db.ForeignKey("exercises.id"), primary_key=True),
+)
 
 
 class Exercise(db.Model):
     __tablename__ = "exercises"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=False)
-    description = Column(Text)
-    category = Column(String(100), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+    category = db.Column(db.String(100), nullable=False)
+    # workouts = db.relationship("Workout", backref="exercise")
 
     def json(self):
         return {
             "id": self.id,
             "name": self.name,
-            "description": self.description,
+            "description": self.description or "No description available",
             "category": self.category,
         }
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    workouts = db.relationship("Workout", backref="user")
+
+    def json(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "name": self.name,
+        }
+
+
+class Workout(db.Model):
+    __tablename__ = "workouts"
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, default=datetime.now(), nullable=False)
+    user_workout_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    comments = db.Column(db.Text)
+
+    exercises = db.relationship(
+        "Exercise", secondary=workout_exercise, backref="workouts"
+    )
+
+    def json(self):
+        return {
+            "id": self.id,
+            "date": self.date.isformat(),
+            "user": self.user.username,
+            "exercises": [exercise.json() for exercise in self.exercises],
+            "comments": self.comments or "",
+        }
+
